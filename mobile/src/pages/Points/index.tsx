5dbbb4fac6ee63fc65c502127aba26dcb68f796a
Vinicius David <vinicius.davidf@gmail.com>
2020-06-04 16:26:50 -0300

    mobile in react native

---
+++ mobile/src/pages/Points/index.tsx
@@ -0,0 +1,273 @@
+import React, { useState, useEffect } from 'react';
+import Constants from 'expo-constants';
+import { Feather as Icon } from '@expo/vector-icons';
+import { View, Text, TouchableOpacity, StyleSheet, ScrollView, Image, Alert } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import MapView, { Marker } from 'react-native-maps';
+import { SvgUri } from 'react-native-svg';
+import * as Location from 'expo-location';
+
+import api from '../../services/api';
+
+interface Category {
+  id: string;
+  title: string;
+  image: string;
+}
+
+interface Point {
+  id: string;
+  image: string;
+  name: string;
+  latitude: number;
+  longitude: number;
+}
+
+interface Params {
+  uf: string;
+  city: string;
+}
+
+const Points = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+  const [points, setPoints] = useState<Point[]>([]);
+
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
+
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  const routeParams = route.params as Params;
+
+  useEffect(() => {
+    async function loadPosition() {
+      const { status } = await Location.requestPermissionsAsync();
+
+      if (status !== 'granted') {
+        Alert.alert('Erro', 'Precisamos da sua permissão de localização')
+        return
+      }
+
+      const location = await Location.getCurrentPositionAsync();
+
+      const { latitude, longitude } = location.coords;
+
+      setInitialPosition([
+        latitude,
+        longitude
+      ])
+    }
+
+    loadPosition();
+  }, []);
+
+  useEffect(() => {
+    api.get('/categories').then(response => {
+      setCategories(response.data);
+    })
+  }, []);
+
+  useEffect(() => {
+    api.get('/points', {
+      params: {
+        city: routeParams.city,
+        uf: routeParams.uf,
+        categories: selectedCategories,
+      }
+    }).then(response => {
+      setPoints(response.data);
+    })
+  }, [selectedCategories]);
+
+  function handleNavigationBack() {
+    navigation.goBack();
+  }
+
+  function handleNavigateToDetail(id: string) {
+    navigation.navigate("Detail", { id });
+  }
+
+  function handleSelectedCategories(id: string) {
+    const checkIfSelected = selectedCategories.findIndex(category => category === id);
+
+    if (checkIfSelected >= 0) {
+      const filteredCategories = selectedCategories.filter(category => category !== id);
+
+      setSelectedCategories(filteredCategories);
+    } else {
+      setSelectedCategories([ ...selectedCategories, id ]);
+    }
+  }
+
+  return (
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigationBack} >
+          <Icon name='arrow-left' size={20} color='#34cb79' />
+        </TouchableOpacity>
+
+        <Text style={styles.title}>Bem vindo</Text>
+        <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
+
+        <View style={styles.mapContainer}>
+          { initialPosition[0] !== 0 && (
+            <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: initialPosition[0],
+              longitude: initialPosition[1],
+              latitudeDelta: 0.014,
+              longitudeDelta: 0.014,
+            }}
+          >
+            {points.map(point => (
+              <Marker
+              key={point.id}
+              style={styles.mapMarker}
+              onPress={() => handleNavigateToDetail(point.id)}
+              coordinate={{
+                latitude: Number(point.latitude),
+                longitude: Number(point.longitude),
+              }}
+              >
+                <View style={styles.mapMarkerContainer}>
+                  <Image
+                    style={styles.mapMarkerImage}
+                    source={{ uri: point.image }}
+                  />
+                  <Text style={styles.mapMarkerTitle}>{point.name}</Text>
+                </View>
+              </Marker>
+            ))}
+          </MapView>
+          ) }
+        </View>
+      </View>
+
+      <View style={styles.itemsContainer} >
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 20 }}
+        >
+          {categories.map(category => (
+            <TouchableOpacity
+              key={category.id}
+              style={[
+                styles.item,
+                selectedCategories.includes(category.id) ? styles.selectedItem : {}
+              ]}
+              onPress={() => handleSelectedCategories(category.id)}
+              activeOpacity={0.65}
+            >
+              <SvgUri width={42} height={42} uri={category.image} />
+              <Text style={styles.itemTitle}>{category.title}</Text>
+          </TouchableOpacity>
+          ))}
+
+        </ScrollView>
+      </View>
+    </>
+  );
+};
+
+export default Points;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 32,
+    paddingTop: 20 + Constants.statusBarHeight,
+  },
+
+  title: {
+    fontSize: 20,
+    fontFamily: 'Ubuntu_700Bold',
+    marginTop: 24,
+  },
+
+  description: {
+    color: '#6C6C80',
+    fontSize: 16,
+    marginTop: 4,
+    fontFamily: 'Roboto_400Regular',
+  },
+
+  mapContainer: {
+    flex: 1,
+    width: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginTop: 16,
+  },
+
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+
+  mapMarker: {
+    width: 90,
+    height: 80,
+  },
+
+  mapMarkerContainer: {
+    width: 90,
+    height: 70,
+    backgroundColor: '#34CB79',
+    flexDirection: 'column',
+    borderRadius: 8,
+    overflow: 'hidden',
+    alignItems: 'center'
+  },
+
+  mapMarkerImage: {
+    width: 90,
+    height: 45,
+    resizeMode: 'cover',
+  },
+
+  mapMarkerTitle: {
+    flex: 1,
+    fontFamily: 'Roboto_400Regular',
+    color: '#FFF',
+    fontSize: 13,
+    lineHeight: 23,
+  },
+
+  itemsContainer: {
+    flexDirection: 'row',
+    marginTop: 16,
+    marginBottom: 32,
+  },
+
+  item: {
+    backgroundColor: '#fff',
+    borderWidth: 2,
+    borderColor: '#eee',
+    height: 120,
+    width: 120,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingTop: 20,
+    paddingBottom: 16,
+    marginRight: 8,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    textAlign: 'center',
+  },
+
+  selectedItem: {
+    borderColor: '#34CB79',
+    borderWidth: 2,
+    backgroundColor: '#eeffee',
+  },
+
+  itemTitle: {
+    fontFamily: 'Roboto_400Regular',
+    textAlign: 'center',
+    fontSize: 13,
+  },
+});
